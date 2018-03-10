95dfcdef0e003ea474eec22d1c9fbc4b8deba649
Philipe Louchtch <philipelouchtch@outlook.com>
2018-03-10 17:44:07 +0100

    made dict and corpus for 8cc, can make it crash now

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-AFL_PRELOAD=/usr/local/lib/afl/libdislocator.so afl-2.52b/afl-fuzz -T xml.c -x afl-2.52b/dictionaries/xml.dict -i corpus -o results -- ./fuzz @@
+
+# usage: $:./run.sh <path to binary> <dictionary> <corpus>
+AFL_PRELOAD=/usr/local/lib/afl/libdislocator.so afl-2.52b/afl-fuzz -T ${1} -x ${2} -i ${3} -o results -- ${1} @@
