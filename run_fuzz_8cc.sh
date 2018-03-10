ca1cb2fa86d0bf019c01b599b7dab02554a4ce34
Philipe Louchtch <philipelouchtch@outlook.com>
2018-03-10 22:49:42 +0100

    reorganized corpus dir more tidy on the repository root level

--- run_fuzz_8cc.sh
+++ run_fuzz_8cc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # usage: $:./run.sh <path to binary> <dictionary> <corpus>
-AFL_PRELOAD=/usr/local/lib/afl/libdislocator.so afl-2.52b/afl-fuzz -T 8cc -x c.dict -i corpus_ccode -o results -- 8cc/8cc -c @@ -o /dev/null
+AFL_PRELOAD=/usr/local/lib/afl/libdislocator.so afl-2.52b/afl-fuzz -T 8cc -x c.dict -i corpus/c/ -o results -- 8cc/8cc -c @@ -o /dev/null
